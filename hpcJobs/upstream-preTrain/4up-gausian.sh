db9d6f945aa07df04ff830bc404855f403d9fb62
fbison <fbisons107@outlook.com>
2025-09-03 21:29:10 -0300

    chore: pre train after optune search for 3 and 4

---
+++ hpcJobs/upstream-preTrain/4up-gausian.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --partition=SP2
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=20000 
+#SBATCH -J ic4UpsGaus
+#SBATCH --time=192:00:00
+#SBATCH -o out4UpsGaus.txt
+#SBATCH -e err4UpsGaus.txt
+
+# 1. Carregar o módulo base do Miniconda
+module load Miniconda/biopython
+
+# 2. Criar ou ativar um ambiente virtual dedicado
+conda create -n my_optuna_env --clone Miniconda/biopython --yes
+conda activate my_optuna_env
+
+# 3. Instalar as dependências no seu ambiente virtual
+pip install -r requirements.txt
+
+# 4. Executar o seu script Python
+python transfer_learn_net.py model=ft_transformer_pretrain_up4_gaussian hyp=hyp_pretrain_up4_gaussian dataset=ic_upstream4_Imputation_Gaussian_exp_100_1
+
+# 5. (Opcional) Desativar o ambiente após a execução
+conda deactivate
